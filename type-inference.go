9fd93e7b68eab75d708425153258b2d63425a400
Reisuke1231 <reisuke1231@gmail.com>
2022-03-28 14:29:40 +0900

    /basics/14

---
+++ type-inference.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42 // change me!
+	y := 3.141592
+	z := 0.123 + 5i
+	fmt.Printf("%T: %v\n", x, x)
+	fmt.Printf("%T: %v\n", y, y)
+	fmt.Printf("%T: %v\n", z, z)
+}

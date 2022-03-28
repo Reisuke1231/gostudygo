36ed352cd0c55901a28f798533923bcb2a11d419
Reisuke1231 <reisuke1231@gmail.com>
2022-03-28 11:42:35 +0900

    Add area right answer

--- struct_square.go
+++ struct_square.go
@@ -2,36 +2,58 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
+// Point is a 2d point
 type Point struct {
 	X int
 	Y int
 }
 
+// Move moves the point
 func (p *Point) Move(dx int, dy int) {
 	p.X += dx
 	p.Y += dy
 }
 
-func (s *Square) Area() int {
-	return s.Length * s.Length
-}
-
+// Square is a square
 type Square struct {
 	Center Point
 	Length int
 }
 
+// NewSquare returns a new square
+func NewSquare(x int, y int, length int) (*Square, error) {
+	if length <= 0 {
+		return nil, fmt.Errorf("length must be > 0")
+	}
+
+	s := &Square{
+		Center: Point{x, y},
+		Length: length,
+	}
+
+	return s, nil
+}
+
+// Move moves the square
+func (s *Square) Move(dx int, dy int) {
+	s.Center.Move(dx, dy)
+}
+
+// Area returns the square are
+func (s *Square) Area() int {
+	return s.Length * s.Length
+}
+
 func main() {
-	p := Point{2, 3}
-	fmt.Printf("%v\n", p)
-	p.Move(3, 3)
-	fmt.Printf("%v\n", p)
+	s, err := NewSquare(1, 1, 10)
+	if err != nil {
+		log.Fatalf("ERROR: can't create square")
+	}
 
-	s := Square{p, 10}
-	fmt.Printf("%v\n", s)
-	area := s.Area()
-	fmt.Printf("%v\n", area)
-
+	s.Move(2, 3)
+	fmt.Printf("%+v\n", s)
+	fmt.Println(s.Area())
 }
